da2d72f2dfa25c0d3f1e753ab1ce9764c8c539ab
NatachaIrken <victoria1236@gmail.com>
2022-02-28 19:07:02 -0300

    App descuentos

--- contrains/contrains/ViewController.swift
+++ contrains/contrains/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var bottomConstrain: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pantalla()
+
     }
 
+    
+    func pantalla(){
+        if UIDevice().userInterfaceIdiom == .phone {
+            switch UIScreen.main.nativeBounds.height{
+            case 1136:
+                print("Iphone 5 o SE")
+            case 1334:
+                print("Iphone 6 6S 7 8")
+            case 1920:
+                print("Plus")
+            case 2436:
+                print("Iphone X XS")
+                self.bottomConstrain.constant = 300
+            case 1792:
+                print("Iphone XR")
+            case 2688:
+                print("Iphone XS MAX")
+            default:
+                print("Cualquier otro tamaño")
+            
+                
+                
+            }
+        }
+    }
 
 }
 
